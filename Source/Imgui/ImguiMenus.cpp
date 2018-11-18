795c7c3e5a68582e2e319b4f64342ae2554c3d1c
Chris Varnsverry <chris.varnsverry@virtualarts.co.uk>
2018-11-18 12:31:42 +0000

    Fix the menus sharing the same name

--- Source/Imgui/ImguiMenus.cpp
+++ Source/Imgui/ImguiMenus.cpp
@@ -5,37 +5,37 @@
 namespace ImGui::Integration
 {
 
-static bool g_cameraWindowOpen = false;
-static bool g_lightingWindowOpen = false;
-
 float g_Controls_Camera_Speed = 0.01f;
 float g_Controls_Camera_Sensitivity = 0.001f;
 
-float g_Lighting_Ambient[3] = { 0.2f, 0.2f, 0.2f };
-
-void RenderCameraControlMenu(bool* pOpen)
+void RenderCameraMenu(bool* pOpen)
 {
 	ImGui::SetNextWindowSize(ImVec2(350, 560), ImGuiSetCond_FirstUseEver);
 
-	if (ImGui::Begin("Controls: Camera", pOpen))
+	if (ImGui::Begin("Camera", pOpen))
 	{
 		ImGui::SliderFloat("Speed", &g_Controls_Camera_Speed, 0.0f, 0.05f);
 		ImGui::SliderFloat("Sensitivity", &g_Controls_Camera_Sensitivity, 0.0f, 0.01f);
+		ImGui::End();
 	}
 
-	ImGui::End();
 }
+
+float g_Lighting_Ambient[3] = { 0.2f, 0.2f, 0.2f };
 
 void RenderLightingWindow(bool* pOpen) {
 	ImGui::SetNextWindowSize(ImVec2(350, 560), ImGuiSetCond_FirstUseEver);
 
-	if (ImGui::Begin("Controls: Camera", pOpen))
+	if (ImGui::Begin("Lighting", pOpen))
 	{
 		ImGui::ColorEdit3("Ambient Color", g_Lighting_Ambient);
+		ImGui::End();
 	}
 
-	ImGui::End();
 }
+
+static bool g_cameraWindowOpen = false;
+static bool g_lightingWindowOpen = false;
 
 void RenderMainMenu()
 {
@@ -56,7 +56,7 @@ void RenderMenus()
 {
 	RenderMainMenu();
 
-	if(g_cameraWindowOpen) RenderCameraControlMenu(&g_cameraWindowOpen);
+	if(g_cameraWindowOpen) RenderCameraMenu(&g_cameraWindowOpen);
 	if (g_lightingWindowOpen) RenderLightingWindow(&g_lightingWindowOpen);
 
 }
